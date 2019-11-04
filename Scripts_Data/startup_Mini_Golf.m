66312a21d3ca206f3282d4bae8944e560d12d0a1
Steve Miller <smiller@mathworks.com>
2019-11-04 23:35:00 +0100

    Initial commit R2019b. Converted to MATLAB project.

---
+++ Scripts_Data/startup_Mini_Golf.m
@@ -0,0 +1,12 @@
+% STARTUP MINI GOLF
+% Copyright 2012-2019 The MathWorks(TM), Inc.
+
+Mini_Golf_Model_HomeDir = pwd;
+
+Mini_Golf_Model_PARAM
+%edit Mini_Golf_Model_PARAM
+Demo_Putter_Adjustments
+edit Demo_Putter_Adjustments
+
+Mini_Golf_Course
+
